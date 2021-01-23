8c0b7822f1dce8a317028e939e48bf28243be7f6
alfredopl <aalfredoplz@gmail.com>
2021-01-23 19:07:36 -0300

    validates , enum type_group

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,6 +1,6 @@
 class Group < ApplicationRecord
-    validates :name, :type, presence: true
-    enum type: [:men, :women, :band]
+    validates :name, :type_group, presence: true
+    enum type_group: [:men, :women, :band]
     has_many :concerts
     has_many :crews
 end
