32769db67da6c16a73cd304c951f14f78fbe91f8
alfredopl <aalfredoplz@gmail.com>
2021-01-24 17:35:04 -0300

    more methods instance

--- app/models/group.rb
+++ app/models/group.rb
@@ -25,5 +25,22 @@ class Group < ApplicationRecord
         people.sum
     end
 
-    
+    def cant_concierts_month
+        time= Time.now.strftime("%Y-%m-01")
+        t =Time.now.strftime("%Y-%m-%d")
+        Conciert.references(:groups).where(group_id: id).where(date: time..t).count
+    end
+    def last_conciert
+        last=Conciert.references(:groups).where(group_id: id).order(date: :desc).first
+        last.date
+    end
+
+    def max_people
+        max = Conciert.references(:groups).where(group_id: id).pluck :people
+        max.max
+    end
+    def max_duration
+        duration = Conciert.references(:groups).where(group_id: id).pluck :duration
+        duration.max.strftime("%Hh:%Mm")
+    end
 end
