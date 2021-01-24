f36fb50679f2c5a39aae82f78093d3ec29fc66d8
alfredopl <aalfredoplz@gmail.com>
2021-01-24 11:17:02 -0300

    methods instance

--- app/models/group.rb
+++ app/models/group.rb
@@ -3,4 +3,26 @@ class Group < ApplicationRecord
     enum type_group: [:men, :women, :band]
     has_many :concerts
     has_many :crews
+    
+    #methods instance
+    #quantity crew
+    def cant_crew
+        Crew.references(:groups).where(group_id: id).count
+    end
+    #first date
+    def date_debut
+       debut =Conciert.references(:groups).where(group_id: id).pluck :date
+       debut.first
+    end
+    #quantity concierts
+    def cant_concierts
+        Conciert.references(:groups).where(group_id: id).count
+    end
+    #quantity people
+    def cant_people
+        people =Conciert.references(:groups).where(group_id: id).pluck :people
+        people.sum
+    end
+
+    
 end
